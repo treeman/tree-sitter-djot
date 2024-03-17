a7cadf86c92acf0c6bd90018501b3b94b4ce509e
Jonas Hietala <mail@jonashietala.se>
2024-03-17 11:05:46 +0100

    Proper Djot -> djot rename

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_Djot::language()).expect("Error loading Djot grammar");
+//! parser.set_language(tree_sitter_djot::language()).expect("Error loading Djot grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +18,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_Djot() -> Language;
+    fn tree_sitter_djot() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_Djot() }
+    unsafe { tree_sitter_djot() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
